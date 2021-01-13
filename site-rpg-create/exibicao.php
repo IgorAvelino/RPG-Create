e544dbf15a75dee4cf43a0399a2a13aa8b13b089
Igor Avelino <igoravelino96@gmail.com>
2021-01-13 18:42:07 -0300

    criacao da pagina de exibicao

---
+++ site-rpg-create/exibicao.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset= "UTF-8">
+    <link rel= stylesheet href="css/estilo.css">
+    <link rel="shortcut icon" href="img/fav.png">
+    <title>Comunidade! Personagens Criados!</title>
+</head>
+<body>
+<div class="interface">
+
+    <!-- code... -->
+
+</div>
+</body>
+</html>
